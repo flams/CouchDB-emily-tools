cd2f67a71f5aeda6acc3e895f2ef7f0a7cf167c5
Olivier Scherrer <pode.fr@gmail.com>
2013-01-01 13:39:08 +0000

    need to add CouchDB Handler to Emily

---
+++ src/CouchDBBase.js
@@ -0,0 +1,13 @@
+/**
+ * https://github.com/flams/CouchDB-emily-tools
+ * The MIT License (MIT)
+ * Copyright (c) 2012 Olivier Scherrer <pode.fr@gmail.com>
+ */
+
+define("CouchDBBase",
+
+["Store", "StateMachine", "Tools", "Promise"],
+
+function CouchDBBase() {
+
+});
--- tools.js
+++ tools.js
@@ -9,4 +9,56 @@ var requirejs = require("requirejs"),
 
 requirejs(__dirname + "/build/CouchDBTools.js");
 
+// Add this to Emily's handlers
+
+		// CouchDB is built-in
+		/*"CouchDB" : function (data, onEnd, onData) {
+
+			var cfg = exports.config.get("CouchDB"),
+				req;
+
+			data.hostname = cfg.hostname;
+			data.port = cfg.port;
+			data.path += "?" + qs.stringify(data.query);
+
+			var exec = function () {
+				req = http.request(data, function (res) {
+
+					var body = "";
+
+					res.on("data", function (chunk) {
+						onData && onData(chunk);
+						body += chunk;
+					});
+
+					res.on("end", function () {
+						onEnd(body);
+					});
+				});
+
+				req.end(data.data, "utf8");
+			};
+
+			if (data.handshake) {
+
+				var cookieJSON = cookie.parse(data.handshake.headers.cookie);
+
+				// I don't like the split but is there a better solution?
+				cfg.sessionStore.get(cookieJSON["suggestions.sid"].split("s:")[1].split(".")[0], function (err, session) {
+					if (err) {
+						throw new Error(err);
+					} else {
+						data.auth = session.auth;
+						exec();
+					}
+				});
+			} else {
+				exec();
+			}
+
+			return function () {
+				req.abort && req.abort();
+			};
+		}*/
+
 module.exports.requirejs = requirejs;
