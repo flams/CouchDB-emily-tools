596a33b49362f622e6c5e4d12307460e963389cd
Olivier Scherrer <pode.fr@gmail.com>
2013-04-01 11:54:49 +0100

    adding CouchDBView

--- specs/CouchDBView.spec.js
+++ specs/CouchDBView.spec.js
@@ -10,14 +10,20 @@ function (CouchDBBase, CouchDBView, Store) {
 
 	describe("CouchDBView inherits from CouchDBBase", function () {
 
-		var couchDBView = new CouchDBView();
-
 		it("should be a constructor function", function () {
 			expect(CouchDBView).toBeInstanceOf(Function);
 		});
 
 		it("should inherit from CouchDBBase", function () {
 			expect(CouchDBView).toBeInstanceOf(CouchDBBase.constructor);
+		});
+
+		it("should initialize with data", function () {
+			var data = {a:10},
+				couchDBView = new CouchDBView(data);
+
+			expect(couchDBView.get("a")).toBe(10);
+
 		});
 
 	});
