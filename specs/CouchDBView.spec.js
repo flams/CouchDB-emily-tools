d9762ced4d2e15df92c0d50266f6fe88f96994bc
Olivier Scherrer <pode.fr@gmail.com>
2013-04-01 11:27:43 +0100

    adding CouchDBView

---
+++ specs/CouchDBView.spec.js
@@ -0,0 +1,15 @@
+/**
+ * https://github.com/flams/CouchDB-emily-tools
+ * The MIT License (MIT)
+ * Copyright (c) 2012-2013 Olivier Scherrer <pode.fr@gmail.com>
+ */
+
+require(["CouchDBBase", "CouchDBView", "Store"],
+
+function (CouchDBBase, CouchDBView, Store) {
+
+	describe("CouchDBView inherits from CouchDBBase", function () {
+
+	});
+
+});
---
+++ src/CouchDBView.js
@@ -0,0 +1,19 @@
+/**
+ * https://github.com/flams/CouchDB-emily-tools
+ * The MIT License (MIT)
+ * Copyright (c) 2012-2013 Olivier Scherrer <pode.fr@gmail.com>
+ */
+
+define("CouchDBView",
+
+["Store", "CouchDBBase"],
+
+/**
+ * @class
+ * CouchDBView synchronizes a Store with a CouchDB view
+ */
+function CouchDBView(Store, CouchDBBase) {
+
+
+
+});
