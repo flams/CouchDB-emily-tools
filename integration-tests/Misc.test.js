da22f92a02d956c2e20c258f0bd364055d28914a
Olivier Scherrer <pode.fr@gmail.com>
2013-05-06 18:42:11 +0100

    adding Misc integration tests

---
+++ integration-tests/Misc.test.js
@@ -0,0 +1,49 @@
+var emily = require("emily"),
+	tools = require("../tools"),
+	http = require("http");
+
+http.globalAgent.maxSockets = 64;
+
+emily.handlers.set("CouchDB", tools.handler);
+
+tools.configuration.adminAuth = "couchdb:couchdb";
+
+function catchError(error) {
+	if (typeof error == "object") {
+		error = JSON.stringify(error);
+	}
+	error && console.error('\u001b[31m' + error + '\u001b[0m');
+}
+
+function success(message) {
+		if (typeof message == "object") {
+		message = JSON.stringify(message);
+	}
+	message && console.log('\u001b[32m' + message + '\u001b[0m')
+}
+
+/**
+ * Tested workflow:
+ * Synchronization with a CouchDBDocument that can't be sent by the server in one frame
+ */
+tools.requirejs(["CouchDBDocument", "Transport"], function (CouchDBDocument, Transport) {
+
+	var couchDBDocument = new CouchDBDocument,
+		transport = new Transport(emily.handlers);
+
+	couchDBDocument.setTransport(transport);
+
+	couchDBDocument.sync("test", "bigDocument")
+
+	.then(function () {
+		this.watchValue("long field", function () {
+			console.log(arguments)
+		})
+	}, couchDBDocument, catchError);
+
+
+});
+
+
+process.on('uncaughtException', catchError);
+
