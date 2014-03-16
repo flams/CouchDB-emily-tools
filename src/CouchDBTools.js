4849a402e98d0af5397198e08a204256dd38927c
Olivier Scherrer <pode.fr@gmail.com>
2014-03-16 18:02:20 -0400

    add CouchDBTools that lists all the modules

---
+++ src/CouchDBTools.js
@@ -0,0 +1,12 @@
+/**
+ * https://github.com/flams/CouchDB-emily-tools
+ * The MIT License (MIT)
+ * Copyright (c) 2012-2014 Olivier Scherrer <pode.fr@gmail.com>
+ */
+module.exports = {
+	CouchDBBulkDocuments: require("./CouchDBBulkDocuments"),
+	CouchDBDocument: require("./CouchDBDocument"),
+	CouchDBView: require("./CouchDBView"),
+	CouchDBSecurity: require("./CouchDBSecurity"),
+	CouchDBUser: require("./CouchDBUser")
+};
