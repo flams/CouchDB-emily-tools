1e13adc6fdf50d6e524fa37f9eafbd66c2b8cd9e
Olivier Scherrer <pode.fr@gmail.com>
2014-06-15 19:02:51 -0400

    update CouchDBDocument tests

--- test/CouchDBDocument.spec.js
+++ test/CouchDBDocument.spec.js
@@ -8,6 +8,7 @@
     serverTools = require("../tools"),
     Transport = require("transport"),
     Store = require("observable-store"),
+    Promise = require("emily").Promise,
 	expect = chai.expect;
 
 describe("Given CouchDBDocument and a transport", function () {
@@ -17,19 +18,77 @@ describe("Given CouchDBDocument and a transport", function () {
         "CouchDBChange": serverTools.changeHandler
     }));
 
-
     describe("When initialized", function () {
-        var sut = new CouchDBDocument();
+        var sut = new CouchDBDocument(),
+            documentName = "document-" + Math.random();
 
-        it("is a simple observable data-store", function () {
-            sut.set("test", true);
+        it("Then is a simple observable data-store", function () {
+            sut.set("name", documentName);
 
-            expect(sut.get("test")).to.be.true;
+            expect(sut.get("name")).to.equal(documentName);
         });
 
-        describe("When synchronized with a CouchDBDocument", function () {
+        describe("When synchronized with a document in CouchDB", function () {
+            sut.setTransport(transport);
+            var promise = sut.sync("test", "document");
 
-        
+            var uploadedPromise = promise.then(function () {
+                sut.set("name", documentName);
+                return sut.upload();
+            });
+
+            describe("When synchronizing another CouchDBDocument on the same document", function () {
+                var anotherDocument = new CouchDBDocument();
+                anotherDocument.setTransport(transport);
+                var nextTest = new Promise();
+
+                var uploadedDocument = uploadedPromise.then(function () {
+                     return anotherDocument.sync("test", "document");
+                });
+
+                it("Then retrieves the previously uploaded document", function (done) {
+                    uploadedDocument.then(function () {
+                        expect(anotherDocument.get("name")).to.equal(documentName);
+                        nextTest.fulfill();
+                        done();
+                    });
+                });
+
+                describe("When the document changes", function () {
+                    var onUpdate = null,
+                        updatedDocumentName = "updatedDocument" + Math.random();
+
+                    var uploadedDocument = nextTest.then(function () {
+                        sut.watchValue("name", function () {
+                            onUpdate = arguments;
+                        });
+                        anotherDocument.set("name", updatedDocumentName);
+                        return anotherDocument.upload();
+                    });
+
+                    it("Then updates the CouchDBDocument", function (done) {
+                        uploadedDocument.then(function () {
+                            expect(onUpdate[0]).to.equal(updatedDocumentName);
+                            expect(onUpdate[1]).to.equal("updated");
+                            done();
+                        });
+                    });
+
+                    describe("When the document is removed", function () {
+                        var removedDocument = uploadedDocument.then(function () {
+                            return anotherDocument.remove();
+                        });
+
+                        it("Then empties the CouchDBDocument", function (done) {
+                            removedDocument.then(function () {
+                                expect(sut.get("name")).to.be.undefined;
+                                done();
+                            });
+                        });
+                    });
+                });
+            });
         });
     });
+
 });
